43a15ebc7d6316f11e8cf0ed47d603928ccd64d9
DloftonSS <derek.lofton1@gmail.com>
2021-06-28 13:27:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setMovieReview] = useState("");
+  const [movieReviewList, setMovieList] = useState([]);
+  const [newReview, setNewReview] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://mysql-deploy-movieapp.herokuapp.com/api/get").then(
+      (response) => {
+        setMovieList(response.data);
+      }
+    );
+  });
+
+  const submitReview = () => {
+    console.log("CLICKED");
+
+    Axios.post("https://mysql-deploy-movieapp.herokuapp.com/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      setMovieList([
+        ...movieReviewList,
+        { movieName: movieName, movieReview: review },
+      ]);
+    });
+  };
+
+  const deleteReview = (movie) => {
+    Axios.delete(
+      `https://mysql-deploy-movieapp.herokuapp.com/api/delete/${movie}`
+    );
+  };
+
+  const updateReview = (movie) => {
+    Axios.put("https://mysql-deploy-movieapp.herokuapp.com/api/update", {
+      movieName: movie,
+      movieReview: newReview,
+    });
+    setNewReview("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Reviews</h1>
+      <div className="form">
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => setMovieName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => setMovieReview(e.target.value)}
+        />
+
+        <button onClick={submitReview}>Submit</button>
+
+        {movieReviewList.map((val) => {
+          return (
+            <div className="card">
+              <h1> {val.movieName}</h1>
+              <p> {val.movieReview}</p>
+              <button
+                onClick={() => {
+                  deleteReview(val.movieName);
+                }}
+              >
+                Delete
+              </button>
+
+              <input
+                type="text"
+                id="updateInput"
+                onChange={(e) => {
+                  setNewReview(e.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  updateReview(val.movieName);
+                }}
+              >
+                Update
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
